91c2954dbfc7edf2440f40360421bdab02ad2816
Debabrata24 <mr.debabrtapc2006@gmail.com>
2025-10-04 21:20:03 +0530

    oth

--- src/script.js
+++ src/script.js
@@ -1,11 +1,23 @@
-function light(){
-    document.getElementById("bodymain").className = "dark:bg-white dark:text-black"
-    document.getElementById("sunIcon").classList.toggle("hidden");
-    document.getElementById("moonIcon").classList.toggle("opacity-100");
+
+const light = document.getElementById("sunIcon");
+const dark = document.getElementById("moonIcon");
+const bodymain = document.getElementById("bodymain");
+
+
+function toggole() {
+    light.classList.toggle("hidden");
+    light.classList.toggle("block");
+
+    dark.classList.toggle("hidden");
+    dark.classList.toggle("block");
+    if (bodymain.classList.contains("dark:bg-black")) {
+        bodymain.classList.remove("dark:bg-black", "dark:text-white");
+        bodymain.classList.add("dark:bg-white", "dark:text-black")
+    }
+    else {
+        bodymain.classList.remove("dark:bg-white", "dark:text-black")
+        bodymain.classList.add("dark:bg-black", "dark:text-white");
+    }
+
 }
 
-function dark(){
-    document.getElementById("bodymain").className = "dark:bg-black dark:text-white"
-    document.getElementById("sunIcon").classList.toggle("visible");
-    document.getElementById("moonIcon").classList.toggle("hidden");
-}
