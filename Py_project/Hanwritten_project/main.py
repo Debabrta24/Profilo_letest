5a25fe62f4925aa881329d0d770175999557ac3e
Debabrata24 <mr.debabrtapc2006@gmail.com>
2025-10-29 21:52:16 +0530

    oth

---
+++ Py_project/Hanwritten_project/main.py
@@ -0,0 +1,10 @@
+# convert text to handwritting using pywordkit
+
+import pywhatkit as pw
+
+text="""hiii
+            hello"""
+            
+            
+pw.text_to_handwriting(text,"Demo.png",[0,0,138])
+print("end")
